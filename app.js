ccc1cce8898e00711db5b107184ac456527c568c
Bael-666 <murder.insane@gmail.com>
2022-12-21 20:42:09 -0600

    Adding security to API

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const connectDatabase = require('./config/database');
 const errorMiddleware = require('./middlewares/errors');
 const errorHandler = require('./utils/errorHandler');
 const cookieParser = require('cookie-parser');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
 
 dotenv.config({path: './config/config.env'});
 
@@ -21,6 +27,9 @@ process.on('uncaughtException', err => {
 //Connecting to database
 connectDatabase();
 
+//Setup security headers
+app.use(helmet());
+
 //Setup body parser
 app.use(express.json());
 
@@ -29,6 +38,28 @@ app.use(cookieParser());
 
 //Handle file uploads
 app.use(fileUpload());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Prevent XSS attacks
+app.use(xssClean());
+
+//Prevent parameter pollution
+app.use(hpp({
+    whitelist : ['positions']
+}));
+
+//Rate limiting
+const limiter = rateLimit({
+    windowMs: 10*60*1000, //10 mins
+    max: 100
+});
+
+//Setup CORS - Accessible by other domains
+app.use(cors());
+
+app.use(limiter);
 
 //Importing all routes
 const jobs = require('./routes/jobs');
